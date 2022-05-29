51d2e3c3604b30f955abaea2c199f523977bc7d9
Yuwei Gu <1652423059@qq.com>
2022-05-29 14:02:45 +0800

    Add files via upload

--- jd-docker.sh
+++ jd-docker.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-echo ">>> "$(date +%F%n%T) "Updating apt...... \n"
-apt update
+echo ">>> "$(date +%F%n%T) "Initializing Environment...... \n"
+
+swapoff -a
+rm -f /swapfile
+
+docker image prune -a -f
+docker rm -f jd1
+docker rm -f `docker ps -q`
+docker rmi -f `docker images -q`
+docker volume rm -f `docker volume ls -q`
+
+snap set system refresh.retain=2
+
+apt -y autoremove --purge
+apt clean -y
+
+rm -rf /etc/apt/sources.list.d
+
+apt update -y
+
 echo ">>> "$(date +%F%n%T) "Update apt Successfully! \n"
-echo ">>> "$(date +%F%n%T) "Installing git...... \n"
-apt -y install git
-echo "\n>>> "$(date +%F%n%T) "Install git Successfully! \n"
 echo ">>> "$(date +%F%n%T) "Installing docker...... \n"
-apt -y install runc
-apt -y install containerd
-apt -y install docker
-apt -y install docker.io
+apt -y install runc containerd docker docker.io docker-compose
 echo "\n>>> "$(date +%F%n%T) "Install docker Successfully! \n"
-echo ">>> "$(date +%F%n%T) "Installing docker-compose...... \n"
-apt -y install docker-compose
-echo "\n>>> "$(date +%F%n%T) "Install docker-compose Successfully! \n"
 echo ">>> "$(date +%F%n%T) "Starting docker Service...... "
 systemctl enable docker
 systemctl restart docker
 echo ">>> "$(date +%F%n%T) "Docker Service Starts Successfully! \n"
 echo ">>> "$(date +%F%n%T) "Initializing JD-Scripts-Docker...... \n"
+
 cd /root/
-
 rm -rf ./jd-scripts-docker/
-
 git clone https://github.com/gywgyw216216/jd-scripts-docker.git
-
 cd jd-scripts-docker
 
-docker rm -f jd1
-docker rm -f `docker ps -q`
-docker rmi -f `docker images -q`
-docker volume rm -f `docker volume ls -q`
 echo "\n>>> "$(date +%F%n%T) "Initialize JD-Scripts-Docker Successfully! \n"
 echo ">>> "$(date +%F%n%T) "Building JD-Scripts-Docker...... \n"
 docker-compose up --build --force-recreate --detach jd1
